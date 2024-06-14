b94d45eb6fce37a5dfc94eb7489f997fdbc0d0ad
Edmeia2025 <neiavini2024@gmail.com>
2024-06-13 21:37:16 -0300

    Add files via upload

---
+++ js/calculadora-simple.js
@@ -0,0 +1,18 @@
+function calcular() {
+    let valor1,valor2,resultado,operacao,caixaResultado;
+    
+        valor1 = Number(document.getElementById("valor1").value);
+        valor2 =Number(document.getElementById(valor2).value);
+        resultado =Number(document.getElementById(valor2).value);
+        operacao =Number(document.getElementById(valor2).value);
+        caixaResultado =(document.getElementById(valor1)).value;
+
+        if (operacao ==="+") {
+            resultado valor1 + valor2
+            else if(operacao === "-"){
+                resultado =valor1 -valor2
+            }
+        }
+    }
+
+
